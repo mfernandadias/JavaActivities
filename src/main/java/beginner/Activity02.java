da17a2cd539121092ccdaa5a25d364cdf061c205
Fernanda Dias <mariadosantodias@gmail.com>
2025-05-19 12:27:01 -0300

    activity 02

---
+++ src/main/java/beginner/Activity02.java
@@ -0,0 +1,19 @@
+package beginner;
+
+public class Activity02 {
+    static int valor = 10;
+
+    public static void main(String[] args){
+        Activity02 opcao = new Activity02();
+
+        System.out.println(valor);
+    }
+
+}
+
+
+/*
+Escreva um programa que declare variáveis locais e globais (dentro de uma classe).
+Inicialize e exiba o valor de ambas as variáveis no console.
+
+ */
