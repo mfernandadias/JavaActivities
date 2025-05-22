5fccdd76a14b7b239ca0aedd6d2855781ac7ac33
Fernanda Dias <mariadosantodias@gmail.com>
2025-05-21 21:21:09 -0300

    activity

---
+++ src/main/java/beginner/Activity05.java
@@ -0,0 +1,24 @@
+package beginner;
+
+public class Activity05 {
+    public static void main(String[] args){
+        /*
+        desenvolva um programa que leia três números inteiros e exiba o maior deles
+         */
+        int a = 100;
+        int b = 90;
+        int c = 9;
+        int maior;
+
+            maior = a;
+            if(b > a){
+                maior = b;
+                System.out.println(maior);
+            }else if(c > a){
+                System.out.println(maior);
+            }else{
+                System.out.println(maior);
+            }
+
+    }
+}
