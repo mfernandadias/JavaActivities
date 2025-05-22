96d233b839379316f154bd06615cda831285ed99
Fernanda Dias <mariadosantodias@gmail.com>
2025-05-21 21:20:57 -0300

    activity 04

---
+++ src/main/java/beginner/Activity04.java
@@ -0,0 +1,12 @@
+package beginner;
+
+public class Activity04 {
+    public static void main(String[] args){
+        System.out.println(Math.pow(2, 4));
+    }
+}
+/*
+Escreva um programa que leia dois números do usuário e exiba o resultado da potenciação do
+primeiro número elevado ao segundo número(use o mátodo Math.pow)
+Dica: pow recebe dois argumentos, o primeiro a base e o segundo o expoente
+ */
