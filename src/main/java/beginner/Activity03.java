c2b398c0f5716aa7aa04547fc9bb65b897991509
Fernanda Dias <mariadosantodias@gmail.com>
2025-05-21 21:20:43 -0300

    activity 08

---
+++ src/main/java/beginner/Activity03.java
@@ -0,0 +1,15 @@
+package beginner;
+
+public class Activity03 {
+    public static void main(String[] args){
+        double valor = 10.9087;
+
+        System.out.println("%.2f%n"+ valor);
+    }
+}
+
+
+/*
+desenvolva um programa que exiba o valor de uma variavel double com duas casa decimais.
+Utilize formatação para garantir que o valor seja exibido corretamente
+ */
