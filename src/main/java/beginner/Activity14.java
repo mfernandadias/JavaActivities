c8c6c2053ce6c394705cf31bf30720501112b1b8
Fernanda Dias <mariadosantodias@gmail.com>
2025-05-21 21:23:39 -0300

    acticity

---
+++ src/main/java/beginner/Activity14.java
@@ -0,0 +1,13 @@
+package beginner;
+
+public class Activity14 {
+    /*
+    ordem decrescente de 10 a 1
+
+     */
+    public static void main(String[] args){
+        for(int i = 10; i > 0; i--){
+            System.out.println(i);
+        }
+    }
+}
