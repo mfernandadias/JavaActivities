d6d982a0508655a289611e0112f2a47e5a495e9b
Fernanda Dias <mariadosantodias@gmail.com>
2025-05-21 21:23:27 -0300

    activity 13

---
+++ src/main/java/beginner/Activity13.java
@@ -0,0 +1,14 @@
+package beginner;
+
+public class Activity13 {
+    public static void main(String[] args){
+        /*
+
+         */
+        int idade1 = 10;
+        int idade2 = 90;
+        int idade3 = 9;
+
+        System.out.println("Maior idade: " + Math.max(Math.max(idade2, idade3), idade1));
+    }
+}
