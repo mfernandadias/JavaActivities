8db27a1def2697f33ed34cdb9ca658b33a2f68aa
Fernanda Dias <mariadosantodias@gmail.com>
2025-05-21 21:25:41 -0300

    activity 21

---
+++ src/main/java/beginner/Activity21.java
@@ -0,0 +1,16 @@
+package beginner;
+
+import java.util.Scanner;
+
+public class Activity21 {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int num;
+
+        do {
+            System.out.println("Digite um valor");
+             num = scan.nextInt();
+        }while(num != 0);
+
+    }
+}
