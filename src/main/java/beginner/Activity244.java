eca796a678852b206b371848336a34061ddc41e7
Fernanda Dias <mariadosantodias@gmail.com>
2025-05-21 21:26:20 -0300

    activity 244

---
+++ src/main/java/beginner/Activity244.java
@@ -0,0 +1,17 @@
+package beginner;
+
+public class Activity244 {
+    public static void main(String[] args){
+        int valor1;
+        int valor2;
+
+    }
+}
+
+/*
+exercicio 64
+Crie um programa que exiba um menu de opções e permita ao usuário
+escolher uma ação (como somar dois números, subtrair, etc.). O menu deve continuar
+sendo exibido até o usuário escolher a opção de sair.
+
+ */
