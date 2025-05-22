f9a48af6f455bdc216ae1a16683ce9523de5269b
Fernanda Dias <mariadosantodias@gmail.com>
2025-05-21 21:22:55 -0300

    acticity 11

---
+++ src/main/java/beginner/Activity11.java
@@ -0,0 +1,21 @@
+package beginner;
+
+public class Activity11 {
+    public static void main(String[] args){
+        /*
+        Crie um programa que demonstre a diferença entre o pré-incremento
+         (++variavel) e o pós-incremento (variavel++). Utilize exemplos práticos e exiba os resultados
+          no console.
+         */
+        int a = 10;
+
+        ++a;
+        System.out.println(a);
+
+        a++;
+        System.out.println(a);
+
+        ++a;
+        System.out.println(a);
+    }
+}
