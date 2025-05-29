c599981f7a3bcf00ad876f6a8f1215073de11961
Fernanda Dias <mariadosantodias@gmail.com>
2025-05-29 00:14:17 -0300

    Exercicio 02

---
+++ src/main/java/intermediarioRevisao/classesWrappers/Exercicio02.java
@@ -0,0 +1,13 @@
+package intermediarioRevisao.classesWrappers;
+
+public class Exercicio02 {
+    /*
+    receba um número decimal como String, converta para Double e exiba a metade
+     */
+    public static void main(String[] args) {
+        String valor = "133.5";
+        double numeroDouble = Double.parseDouble(valor);
+
+        System.out.println(numeroDouble / 2);
+    }
+}
