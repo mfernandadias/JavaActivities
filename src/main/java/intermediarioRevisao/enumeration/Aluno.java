7d89ec52fb0d6b0fb498b8e8b69d86e0fe86e73f
Fernanda Dias <mariadosantodias@gmail.com>
2025-05-29 00:17:41 -0300

    Aluno

---
+++ src/main/java/intermediarioRevisao/enumeration/Aluno.java
@@ -0,0 +1,35 @@
+package intermediarioRevisao.enumeration;
+
+public class Aluno {
+    private String nome;
+    private Nota nota;
+
+    public Aluno(String nome, Nota nota) {
+        this.nome = nome;
+        this.nota = nota;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Nota getNota() {
+        return nota;
+    }
+
+    public void setNota(Nota nota) {
+        this.nota = nota;
+    }
+
+    @Override
+    public String toString() {
+        return "Aluno{" +
+                "nome='" + nome + '\'' +
+                ", nota=" + nota +
+                '}';
+    }
+}
