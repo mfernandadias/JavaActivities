79d55fb55272e596639ab176987e1643c81e1840
Fernanda Dias <mariadosantodias@gmail.com>
2025-06-10 20:30:10 -0300

    exercicio 03

---
+++ src/main/java/avancado/classesWrappers/Exercicio03.java
@@ -0,0 +1,20 @@
+package avancado.classesWrappers;
+
+public class Exercicio03 {
+    /*
+    crie um array de int[] e um de Integer[]. Atribua valores iguais
+    e mostre a diferença entre eles
+    */
+    public static void main(String[] args){
+        int[] valorInt = {1, 2, 3, 4, 5, 6};
+        Integer[] valor = {1, 2, 3, 4, 5, 6, 9};
+
+        for(int i = 1; i < valorInt.length; i++){
+            System.out.println(valorInt[i]);
+        }
+        System.out.println("  ");
+        for(int i = 0; i < valor.length; i++){
+            System.out.println(valor[i]);
+        }
+    }
+}
