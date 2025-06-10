f76bebb059654a79a3533158a2d2e1b33fe0ae98
Fernanda Dias <mariadosantodias@gmail.com>
2025-06-10 20:30:45 -0300

    Exercicio 06

---
+++ src/main/java/avancado/classesWrappers/Exercicio06.java
@@ -0,0 +1,20 @@
+package avancado.classesWrappers;
+
+import static java.lang.Integer.parseInt;
+
+public class Exercicio06 {
+    public static void main(String[] args){
+        /*
+        leia dois valores String, converta-os para Integer, compare com compareTo e
+        diga qual é maior
+         */
+        String exemplo1 = "12";
+        String exemplo2 = "12";
+        Integer valor1 = parseInt(exemplo1);
+        Integer valor2 = parseInt(exemplo2);
+
+        System.out.println(valor1.compareTo(valor2));
+
+
+    }
+}
