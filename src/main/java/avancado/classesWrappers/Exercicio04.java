00557574d67f7a59974c353fa19d36b733690686
Fernanda Dias <mariadosantodias@gmail.com>
2025-06-10 20:30:30 -0300

    Exercicio 04

---
+++ src/main/java/avancado/classesWrappers/Exercicio04.java
@@ -0,0 +1,15 @@
+package avancado.classesWrappers;
+
+public class Exercicio04 {
+    public static void main(String[] args){
+        /*
+        Use autoboxing para clocar um int em um Integer. Depois,
+        desconverta (unboxing) e exiba
+         */
+        int valor = 10;
+        Integer num = valor;
+
+        valor = num;
+        System.out.println(num);
+    }
+}
