8b2ca0828892df8c2fd13e6838f5093d7c2a02de
Fernanda Dias <mariadosantodias@gmail.com>
2025-08-28 20:34:36 -0300

    conta bancaria

---
+++ src/main/java/aCursoRevisao/secao20/avancado/ContaBancaria.java
@@ -0,0 +1,18 @@
+package aCursoRevisao.secao20.avancado;
+
+public final class ContaBancaria {
+
+    private double saldo;
+
+    public ContaBancaria(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+}
