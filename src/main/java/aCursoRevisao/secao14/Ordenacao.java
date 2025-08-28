383f0e39be36b4bf3d05b5090bba431f10c66ba5
Fernanda Dias <mariadosantodias@gmail.com>
2025-08-28 20:30:45 -0300

    java ordenação

---
+++ src/main/java/aCursoRevisao/secao14/Ordenacao.java
@@ -0,0 +1,12 @@
+package aCursoRevisao.secao14;
+
+import java.util.Arrays;
+
+public class Ordenacao {
+    public static void main(String[] args){
+        int[] numeros = {5, 3, 4,2 ,3 , 5};
+
+        Arrays.sort(numeros); //ordena em ordem creescente
+        System.out.println(Arrays.toString(numeros)); //
+    }
+}
