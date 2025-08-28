31837bdcf42cf066dee8802144ff2e5cf65a60a0
Fernanda Dias <mariadosantodias@gmail.com>
2025-08-28 20:35:44 -0300

    Calculadora Java

---
+++ src/main/java/aCursoRevisao/secao20/Calculadora.java
@@ -0,0 +1,11 @@
+package aCursoRevisao.secao20;
+
+interface  Calculadora {
+
+    int somar(int a, int b);
+
+    //ele pode ser utilizado, sem implementação
+    default int multiplicar(int a, int b){
+        return a * b;
+    }
+}
