eaf1f78ed835dc3078a65c1470e4ab172d4491de
Fernanda Dias <mariadosantodias@gmail.com>
2025-08-28 20:28:39 -0300

    java 01

---
+++ src/main/java/aCursoRevisao/secao10/ContadorDePalavras.java
@@ -0,0 +1,14 @@
+package aCursoRevisao.secao10;
+
+public class ContadorDePalavras {
+    /*
+    1 - obter uma frase do usuário
+    2 - contar as palavras da frase e exibir o resultado
+    3 - solicitar se o usuário quer digitar outra frase
+    4 - Se sim, repetir lógica 1 e 2
+    5 - Se não, encerrar programa
+     */
+
+
+
+}
