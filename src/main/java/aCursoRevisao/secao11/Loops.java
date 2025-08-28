6122b341ad7d342cf9068b0a9e3bbe0bdbac839a
Fernanda Dias <mariadosantodias@gmail.com>
2025-08-28 20:29:07 -0300

    java 03

---
+++ src/main/java/aCursoRevisao/secao11/Loops.java
@@ -0,0 +1,14 @@
+package aCursoRevisao.secao11;
+
+public class Loops {
+    public static void main(String[] args){
+        /*
+        1 - for
+        Loop que vai rodar de 1 a 5
+         */
+
+        for(int i = 0; i <= 5; i++){
+            System.out.println(i);
+        }
+    }
+}
