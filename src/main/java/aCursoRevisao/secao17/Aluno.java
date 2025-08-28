8f39973a3d9ba84115697f5d7b2e21d8f2e6ccdc
Fernanda Dias <mariadosantodias@gmail.com>
2025-08-28 20:31:10 -0300

    aluno Java

---
+++ src/main/java/aCursoRevisao/secao17/Aluno.java
@@ -0,0 +1,9 @@
+package aCursoRevisao.secao17;
+
+public class Aluno {
+    /*
+    crie uma classe Aluno com as propriedades privadas nome, matricula, notaFiscal.
+    Adicione um construtor parametrizado para inicializar essas propriedades e
+    implemente os setters e getters com validação para garantir
+     */
+}
