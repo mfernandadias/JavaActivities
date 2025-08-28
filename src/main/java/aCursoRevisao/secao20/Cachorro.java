9747708bfa2415da35eaa9c2cecdf41978792ede
Fernanda Dias <mariadosantodias@gmail.com>
2025-08-28 20:35:36 -0300

    cachorro Java

---
+++ src/main/java/aCursoRevisao/secao20/Cachorro.java
@@ -0,0 +1,9 @@
+package aCursoRevisao.secao20;
+
+public class Cachorro extends Animal{
+
+    //é obrigado a usar as propriedades da classe pai - super
+    public Cachorro(String nome) {
+        super(nome);
+    }
+}
