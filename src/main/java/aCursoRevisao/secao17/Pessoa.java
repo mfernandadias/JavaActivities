d70d76c899335cd73be3d8115f09fdac16559c2a
Fernanda Dias <mariadosantodias@gmail.com>
2025-08-28 20:33:39 -0300

    Pessoa Java

---
+++ src/main/java/aCursoRevisao/secao17/Pessoa.java
@@ -0,0 +1,23 @@
+package aCursoRevisao.secao17;
+
+public class Pessoa {
+    private String nome;
+
+    public void setNome(String nome){
+        this.nome = nome; //this.nome = refere-ao atributo da classe
+    }
+    /*
+    a palavra-chave this faz referência ao prórpio objeto da classe.
+    É usada quando queremos acessar atributos ou métodos do objeto atual, especialmente
+    quando
+     */
+    /*
+    setters são metodos usados para alterar os valores dos atributos de um objetos
+     */
+    /*
+    Getters são métodos usados para acessar os valores dos atributos de um objeto
+    public int getIdade() {
+       return this.idade;
+    }
+     */
+}
