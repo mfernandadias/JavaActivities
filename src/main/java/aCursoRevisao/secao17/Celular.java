a21d1c55555456aa6ea9f12778b42041ca39f47d
Fernanda Dias <mariadosantodias@gmail.com>
2025-08-28 20:31:54 -0300

    Celular Java

---
+++ src/main/java/aCursoRevisao/secao17/Celular.java
@@ -0,0 +1,12 @@
+package aCursoRevisao.secao17;
+
+public class Celular {
+    /*
+    crie uma classe Celular que tenha as seguintes propriedades: marca. modelo,
+    bateria.
+    Implemente os métodos para ligar e desligar o celular, e outro método que
+    simule o consumo da bateria quando o celular é usado
+    Crie a classe principal para instanciar dois objetos Celular
+    e testar os métodos criados
+     */
+}
