7acc53d867684adc54ca22a801e9038fdbb4e9e8
Fernanda Dias <mariadosantodias@gmail.com>
2025-08-28 20:28:58 -0300

    exercicio java

---
+++ src/main/java/aCursoRevisao/secao11/Exercicio.java
@@ -0,0 +1,12 @@
+package aCursoRevisao.secao11;
+
+public class Exercicio {
+    /*
+    pedir dois números para o usuário (double)
+    Apresentar uma tabela/texto para escolher a operação
+    Resgatar a operação que o usuário seleciou
+    REalizar o calculço
+    Exibir o resultado
+
+     */
+}
