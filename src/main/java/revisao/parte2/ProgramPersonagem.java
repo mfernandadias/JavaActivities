41caaaa70a066bbfe5b8551d1b2db9d83f99e26c
Fernanda Dias <mariadosantodias@gmail.com>
2025-09-08 02:08:16 -0300

    program personagem

---
+++ src/main/java/revisao/parte2/ProgramPersonagem.java
@@ -0,0 +1,20 @@
+package revisao.parte2;
+
+public class ProgramPersonagem {
+    public static void main(String[] args) throws Exception{
+        Personagem heroi = new Personagem();
+
+        heroi.nome = "Hercules";
+        heroi.nivel = 2;
+        heroi.forca = 16;
+
+        // "Personagem: <nome>
+        /*
+        O que é um método em Java ?
+        Um método é um bloco de código que executa uma tarefa específica. Ele
+        pode receber parâmetros de entrada, proce
+         */
+
+
+    }
+}
