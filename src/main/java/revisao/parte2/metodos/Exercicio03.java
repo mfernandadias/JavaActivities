a5ba3dd913d7677c21c256d9ab0ba9517290b3f6
Fernanda Dias <mariadosantodias@gmail.com>
2025-09-08 02:05:07 -0300

    exercicio 03

---
+++ src/main/java/revisao/parte2/metodos/Exercicio03.java
@@ -0,0 +1,18 @@
+package revisao.parte2.metodos;
+
+public class Exercicio03 {
+    /*
+    cire um método que receba um números inteiros e retorne a soma
+     */
+    public int somar(int a, int b){
+        return a + b;
+    }
+
+    public static void main(String[] args){
+        Exercicio03 exemplo = new Exercicio03();
+
+        //exemplo.somar(3, 6);
+
+        System.out.println(exemplo.somar(3, 5));
+    }
+}
