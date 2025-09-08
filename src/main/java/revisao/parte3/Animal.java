8f1d2d8eb458cfd999c47d188f7af14a953dc3ae
Fernanda Dias <mariadosantodias@gmail.com>
2025-09-08 02:08:25 -0300

    revisao Animal

---
+++ src/main/java/revisao/parte3/Animal.java
@@ -0,0 +1,13 @@
+package revisao.parte3;
+
+public class Animal {
+    /*
+    Zoológico Polimórfico: Animal (abstrata) com som();
+    Leao, Tucano, Cobra. Crie lista e toque “orquestra” de sons.
+     */
+    void emitirSonr(){
+        System.out.println("Som genérico");
+    }
+
+
+}
