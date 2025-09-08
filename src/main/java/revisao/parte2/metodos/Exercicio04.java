70b269b3e034dbdf2d20678c2a23c893cc1073fd
Fernanda Dias <mariadosantodias@gmail.com>
2025-09-08 02:05:18 -0300

    exercício 04

---
+++ src/main/java/revisao/parte2/metodos/Exercicio04.java
@@ -0,0 +1,11 @@
+package revisao.parte2.metodos;
+
+public class Exercicio04 {
+    /*
+    crie um método que receba uma String(nome) e exiba uma
+    mensagem de boas-vindas
+     */
+    public void nome(String titulo){
+        System.out.println("Olá " + titulo);
+    }
+}
