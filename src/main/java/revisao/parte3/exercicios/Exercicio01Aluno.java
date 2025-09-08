f4fb51edfbb90989e7131c7223aa9245160defa8
Fernanda Dias <mariadosantodias@gmail.com>
2025-09-08 02:09:16 -0300

    exercicio01 aluno java

---
+++ src/main/java/revisao/parte3/exercicios/Exercicio01Aluno.java
@@ -0,0 +1,27 @@
+package revisao.parte3.exercicios;
+
+public class Exercicio01Aluno {
+    private String nome;
+    private  double nota;
+
+    public Exercicio01Aluno(String nome, double nota) {
+        this.nome = nome;
+        this.nota = nota;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+}
