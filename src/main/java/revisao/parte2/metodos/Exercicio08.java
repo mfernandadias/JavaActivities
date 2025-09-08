c94823aff55ffc878536940877a3404dde54091e
Fernanda Dias <mariadosantodias@gmail.com>
2025-09-08 02:06:10 -0300

    exercício 08

---
+++ src/main/java/revisao/parte2/metodos/Exercicio08.java
@@ -0,0 +1,36 @@
+package revisao.parte2.metodos;
+
+public class Exercicio08 {
+    /*
+   Crie um método que receba o raio de um círculo e retorne sua área.
+     */
+    public void calculo(double raio){
+        double resultado = Math.PI * Math.pow(raio, 2);
+    }
+
+    public static void main(String[] args){
+
+
+
+    }
+}
+
+/*
+package revisao.parte2.metodos;
+
+public class ExercicioAreaCirculo {
+
+    // Método que calcula a área de um círculo
+    public static double calcularArea(double raio) {
+        return Math.PI * Math.pow(raio, 2);
+    }
+
+    public static void main(String[] args) {
+        double r1 = 5.0;
+        double r2 = 10.0;
+
+        System.out.println("Área do círculo de raio " + r1 + " = " + calcularArea(r1));
+        System.out.println("Área do círculo de raio " + r2 + " = " + calcularArea(r2));
+    }
+}
+ */
