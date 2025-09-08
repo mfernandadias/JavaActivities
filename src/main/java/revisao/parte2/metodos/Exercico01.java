10d1cc14ee8928ce1b212484aa92d53520478f19
Fernanda Dias <mariadosantodias@gmail.com>
2025-09-08 02:06:55 -0300

    exercicio 01

---
+++ src/main/java/revisao/parte2/metodos/Exercico01.java
@@ -0,0 +1,18 @@
+package revisao.parte2.metodos;
+
+public class Exercico01 {
+
+    public void ola(){
+        System.out.println("Olá Mundo!");
+    }
+
+    public static void main(String[] args){
+        /*
+        crie um método void que exibe "Olá Mundo!" no console
+       */
+        Exercico01 ola = new Exercico01();
+
+        ola.ola();
+
+    }
+}
