805c5895b576056402c0b21570d110981116cd96
Fernanda Dias <mariadosantodias@gmail.com>
2025-09-08 02:08:55 -0300

    cachorro Java

---
+++ src/main/java/revisao/parte3/Cachorro.java
@@ -0,0 +1,13 @@
+package revisao.parte3;
+
+public class Cachorro extends Animal{
+}
+
+/*
+1. Herança (extends)
+Uma classe pode herdar atributos e métodos de outra com extends.
+Evita repetição de código.
+Uma subclasse pode adicionar novos métodos ou sobrescrever métodos existentes.
+
+👉 Exemplo:
+ */
