e69bc3f91cd16dd6ab88b0b530de7cf5ec3958e8
Fernanda Dias <mariadosantodias@gmail.com>
2025-09-09 20:19:34 -0300

    curso java

---
+++ src/main/java/revisao/parte5/CursoJava.java
@@ -0,0 +1,19 @@
+package revisao.parte5;
+
+import java.util.ArrayList;
+
+public class CursoJava {
+    public static void main(String[] args){
+        ArrayList<String> carro = new ArrayList<>();
+
+        carro.add("HRV");
+        carro.add("Polo");
+        carro.add("Cruze");
+        carro.add("Argo");
+        carro.add("Fusion");
+
+        carro.clear();
+
+
+    }
+}
